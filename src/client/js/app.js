5422612d5398edfec7fcab7e3b310744c88b2792
abhiteja joginipally <abhiteja@gmail.com>
2015-12-06 15:37:50 -0500

    added new dependencies

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,18 +1,15 @@
 var app = angular.module('artemisgg', [
-	"ui.router",
-	"ngAnimate",
-	"ngTouch",
 ]);
 
 // routing
-app.config(function($stateProvider, $urlRouterProvider)  {
-
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider
-        .state('primary', {
-            url: '/',
-            templateUrl: 'views/primary.html',
-            controller: 'mainController'
-        });
-});
+// app.config(function($stateProvider, $urlRouterProvider)  {
+//
+//     $urlRouterProvider.otherwise('/');
+//
+//     $stateProvider
+//         .state('primary', {
+//             url: '/',
+//             templateUrl: 'views/primary.html',
+//             controller: 'mainController'
+//         });
+// });
--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var serveStatic = require('serve-static');
 var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var json = require('express-json');
+var methodOverride = require('method-override');
 
 module.exports = function (app) {
 	app.set('port', process.env.PORT || 3000);
 	app.set('views', __dirname + '/../server/views');
 	app.set('view engine', 'ejs');
-
+	app.use(morgan('dev'));
+	app.use(json());
+	app.use(bodyParser.urlencoded({ extended: true}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
 	app.use(serveStatic(__dirname + '/../public'));
 
 	if(!process.env.DB_USER || !process.env.DB_PASSWORD){
