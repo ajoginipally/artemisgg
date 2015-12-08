964d70889919a41073d77bec567f09b43a1ae973
abhiteja joginipally <abhiteja@gmail.com>
2015-12-08 02:16:51 -0500

    got user list to work, have to finish authentication

--- src/client/js/controllers/userController.js
+++ src/client/js/controllers/userController.js
@@ -5,11 +5,13 @@ app.controller("userController", function($scope, users, api){
 
 var pruneUsers = function(apiUsers){
   var userData = apiUsers.plain();
-  var users = {};
-  for(var i in userData){
-    var s = userData[i];
-
-    speakers[s].push(s);
-  }
-  return speakers;
+  var users = userData
+  console.log(userData);
+  // var users = {};
+  // for(var i in userData){
+  //   var s = userData[i];
+  //
+  //   users[s].push(s);
+  // }
+  return users;
 };
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,3 +1,5 @@
+var respond = require("../lib/respond");
+
 var User = require("../models/User");
 
 module.exports = function (app) {
@@ -7,45 +9,51 @@ module.exports = function (app) {
     res.json("welcome to the artemisgg api!")
   });
 
-  //get all users
-  app.get('/api/user', function(req, res) {
-    User.find(function(err, users) {
-      if (err)
-        res.send(err);
+  app.get('/api/user', function(req,res){
+		User.find({}, function(err, docs){
+			return respond(res, err, docs);
+		});
+	});
 
-      res.json(users);
-    });
-  });
-
-  //create new user
-  app.post('/api/user', function(req, res) {
-    var user = new User(); //new user model
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.admin = req.body.admin;
-
-    user.save(function(err) {
-      if(err)
-        res.send(err);
-      res.json({message: 'User created!'});
-    });
-  })
-
-  //get user by id
-  app.get('/api/users/:id', function(req, res) {
-    User.find({"_id": req.params.id}, function(err, user){
-      if (err)
-        res.send(err);
-      res.json(user);
-    });
-  });
-
-  //delete user by id
-  app.delete('/api/users/:id', function(req, res) {
-    User.remove({_id: req.params.id}, function(err, user){
-      if(err)
-        res.send(err);
-      res.json({message: 'User deleted!'});
-    });
-  });
+  // //get all users
+  // app.get('/api/user', function(req, res) {
+  //   User.find(function(err, users) {
+  //     if (err)
+  //       res.send(err);
+  //
+  //     res.json(users);
+  //   });
+  // });
+  //
+  // //create new user
+  // app.post('/api/user', function(req, res) {
+  //   var user = new User(); //new user model
+  //   user.username = req.body.username;
+  //   user.password = req.body.password;
+  //   user.admin = req.body.admin;
+  //
+  //   user.save(function(err) {
+  //     if(err)
+  //       res.send(err);
+  //     res.json({message: 'User created!'});
+  //   });
+  // })
+  //
+  // //get user by id
+  // app.get('/api/users/:id', function(req, res) {
+  //   User.find({"_id": req.params.id}, function(err, user){
+  //     if (err)
+  //       res.send(err);
+  //     res.json(user);
+  //   });
+  // });
+  //
+  // //delete user by id
+  // app.delete('/api/users/:id', function(req, res) {
+  //   User.remove({_id: req.params.id}, function(err, user){
+  //     if(err)
+  //       res.send(err);
+  //     res.json({message: 'User deleted!'});
+  //   });
+  // });
 }
