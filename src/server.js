ffb58ec5cf9a1eda13808c6c9f4855a9b838c9f9
Jimmy Ly <ljimmy23@gmail.com>
2015-12-12 16:06:55 -0500

    attempting httpless auth using json tokens

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 var express = require('express');
-var http = require('http');
+//var http = require('http');
+var morgan     = require("morgan");
+var bodyparser = require("body-parser");
+var jwt        = require("jsonwebtoken");
+var mongoose   = require("mongoose");
 var app = express();
 
 require('./config/config')(app);
 require('./server/routes/routes.js')(app);
 
-http.createServer(app).listen(app.get('port'), function () {
+/*http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+*/
