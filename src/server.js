65d89a763dcced619ea584c53844838c01f41965
Jimmy Ly <ljimmy23@gmail.com>
2015-12-12 03:44:20 -0500

    dependencies for stateless auth?

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var http = require('http');
+/*var express    = require("express");
+var morgan     = require("morgan");
+var bodyParser = require("body-parser");
+var jwt        = require("jsonwebtoken");
+var mongoose   = require("mongoose");*/
 var app = express();
 
 require('./config/config')(app);
